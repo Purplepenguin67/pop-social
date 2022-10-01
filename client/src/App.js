7bcf46e07a3c444fb0688c00f8d7745963e5fbac
Douglas Snodgrass <snodgrassdouglas@gmail.com>
2022-09-30 21:15:32 -0700

    added style.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import memories from './images/profile-pic.png';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import memories from './images/memories.png';
+import useStyles from './styles';
+
 const App = () => {
+    const classes = useStyles();
     return(
         <Container maxWidth="lg">
             <AppBar position="static" color="inherit">
@@ -10,7 +16,25 @@ const App = () => {
                 <img src={memories} alt="memories" height="60" />
 
             </AppBar>
+        <Grow in>
+            <Container>
+                <Grid container justify="space-between" alignItems="stretch" spacing={3} >
+                    <Grid item xs={12} sm={7}>
 
+                    <Posts />
+
+                    </Grid>
+
+                    <Grid item xs={12} sm={4}>
+
+                        <Form />
+                    </Grid>     
+
+                </Grid>
+
+            </Container>
+
+        </Grow>
         </Container>
 
     );
---
+++ client/src/components/Form/Form.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <h1>FORM</h1>
+
+    );
+
+
+
+}
+
+export default Form; 
