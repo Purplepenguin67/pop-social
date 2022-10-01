be4e55a61cd6f9a9eaf5911af2e405bbb3cd5a35
Douglas Snodgrass <snodgrassdouglas@gmail.com>
2022-09-30 23:33:01 -0700

    added redux reducers

---
+++ client/src/reducers/posts.js
@@ -0,0 +1,18 @@
+export default = (posts = [], action) => {
+    switch (action.type) {
+        case 'FETCH_all':
+            return posts;
+        case 'CREATE':
+            return posts;
+        default:
+            return posts;
+
+
+    }
+
+
+
+
+
+
+}
