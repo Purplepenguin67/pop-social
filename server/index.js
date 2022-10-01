7056aedc767ce7cf7605c7edd2e7be1d43a455f9
Douglas Snodgrass <snodgrassdouglas@gmail.com>
2022-09-30 19:38:00 -0700

    added post route

--- server/index.js
+++ server/index.js
@@ -3,19 +3,23 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://snodgrass69:Myharto1@@cluster0.ggsb9ib.mongodb.net/?retryWrites=true&w=majority'
+app.use('/posts', postRoutes);
 
-const PORT = process.env.PORT || 5000;
+
+const CONNECTION_URL = 'mongodb+srv://snodgrass32:OU67hNwHuyfbRet1@cluster0.ggsb9ib.mongodb.net/?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT|| 3000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => app.listen(PORT, () => console.log('Server running on port: ${PORT}')))
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: ${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
-.catch((error) => console.log(`${error} did not connect`));
-
-mongoose.set('useFindAndModify', false);
+//mongoose.set('useFindAndModify', false);
