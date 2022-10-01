d2eb2c2fff8737e41529911544073b1b343fd101
Douglas Snodgrass <snodgrassdouglas@gmail.com>
2022-09-30 20:07:30 -0700

    uploaded test image"

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import memories from './images/profile-pic.png';
 const App = () => {
     return(
-        <div>
-            <h1>App</h1>
+        <Container maxWidth="lg">
+            <AppBar position="static" color="inherit">
+                <Typography variant="h2" align="center">Memories</Typography>
+                <img src={memories} alt="memories" height="60" />
 
-        </div>
+            </AppBar>
+
+        </Container>
 
     );
 
